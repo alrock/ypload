3fac43af042234bef7bece01aee9e4ae3fca2072
Grigory Bakunov <thebobuk@ya.ru>
2012-10-06 14:09:06 +0400

    add license and setup requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 setup(name='ypload',
-    version='1.0',
+    version='1.1',
     description='Simple script to upload and publish files to Yandex.Disk',
     author='Grigory Bakunov',
     author_email='thebobuk@ya.ru',
     url='http://github.com/bobuk/ypload',
     py_modules=['ydisk'],
     scripts=['ypload'],
+    requires     = [ 'requests'],
+    license      = 'MIT',
 )
