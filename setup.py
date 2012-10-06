1728a2e7dcdfd5857fc8b8ccaa158190cc024fb7
Grigory Bakunov <thebobuk@ya.ru>
2012-10-06 14:18:26 +0400

    oh. I almost forgot how to play with distutils :(

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(name='ypload',
-    version='1.1',
+    version='1.2.1',
     description='Simple script to upload and publish files to Yandex.Disk',
     author='Grigory Bakunov',
     author_email='thebobuk@ya.ru',
     url='http://github.com/bobuk/ypload',
     py_modules=['ydisk'],
     scripts=['ypload'],
-    requires     = [ 'requests'],
+    requires=['requests (>=0.12.0)', ],
     license      = 'MIT',
 )
