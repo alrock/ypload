91dc2896d317853d57dcc8ee15b47691e0c38959
Gregory Sitnin <greg@sitnin.com>
2012-10-09 01:00:53 +0400

    Implemented simple http callback in getKey()

--- ydisk.py
+++ ydisk.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 import sys
 import os
-import requests, urlparse
+import requests
+import urlparse
 try:
     from pyxml2obj import XMLin
     from dateutil.parser import parse as dateparse
 except:
     XMLin = None
 import json
+import BaseHTTPServer
+
+
+class YploadRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed = urlparse.parse_qs(urlparse.urlparse(self.path).query)
+        if "code" in parsed:
+            YploadRequestHandler._code = parsed["code"][0]
+        self.wfile.write("HTTP/1.0 200 OK")
+        self.send_header("Date", self.date_time_string())
+        self.send_header("Server", self.version_string())
+        self.end_headers()
+        self.wfile.write("Done. Please, return back to the terminal.\n")
+        self.wfile.write("Your code is %s" % YploadRequestHandler._code)
+        self.finish()
+
 
 class FileInfo(dict):
     __getattr__ = dict.__getitem__
@@ -33,14 +50,24 @@ class FileInfo(dict):
     def __str__(self):
         return '%(name)s (%(href)s) - %(length)s bytes' % self
 
-    __repr__    = __str__
+    __repr__ = __str__
+
 
 def getKey(YD_APP_ID, YD_APP_SECRET, keyfile):
     if os.path.isfile(keyfile):
         return open(keyfile, 'r').read()
     import webbrowser
     webbrowser.open('https://oauth.yandex.ru/authorize?response_type=code&client_id=' + YD_APP_ID)
-    code = raw_input('Input your code: ').strip()
+
+    YploadRequestHandler._code = None
+    httpd = BaseHTTPServer.HTTPServer(('', 8714), YploadRequestHandler)
+    httpd.handle_request()
+
+    if YploadRequestHandler._code:
+        code = YploadRequestHandler._code
+    else:
+        code = raw_input('Input your code: ').strip()
+
     res = requests.post('https://oauth.yandex.ru/token',
         data = dict(
             grant_type='authorization_code',
